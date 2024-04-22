a02ea5845e0419e65ea7d86134da8c05fea01c1a
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2024-04-22 15:06:51 +0200

    Updatated the whole code for pushing the numbers and operators to make it more simple and take less lines

--- js/javascript.js
+++ js/javascript.js
@@ -7,80 +7,18 @@ let firstNbr
 let secondNbr
 let operator
 
-//va chercher toutes les valeurs des boutton
-let number0 = document.getElementById('0').value
-let number1 = document.getElementById('1').value
-let number2 = document.getElementById('2').value
-let number3 = document.getElementById('3').value
-let number4 = document.getElementById('4').value
-let number5 = document.getElementById('5').value
-let number6 = document.getElementById('6').value
-let number7 = document.getElementById('7').value
-let number8 = document.getElementById('8').value
-let number9 = document.getElementById('9').value
-
 let operatorPlus = '+'
 let operatorMinus = '-'
 let operatorMult = 'x'
 let operatorDiv = '/'
 
-
 //push les chiffres dams userNumber est affiche sur le display
-function nbr0(){
-    userNumber.push(number0)
+function usrInpNbr(val){
+    userNumber.push(val)
     outputDisplay.innerText = userNumber.join('')
     console.log(userNumber)
 }
-function nbr1(){
-    userNumber.push(number1)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr2(){
-    userNumber.push(number2)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr3(){
-    userNumber.push(number3)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr4(){
-    userNumber.push(number4)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr5(){
-    userNumber.push(number5)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr6(){
-    userNumber.push(number6)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr7(){
-    userNumber.push(number7)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr8(){
-    userNumber.push(number8)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function nbr9(){
-    userNumber.push(number9)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
-function dot(){
-    userNumber.push(dotOp)
-    outputDisplay.innerText = userNumber.join('')
-    console.log(userNumber)
-}
+
 
 //reset tous les input ainsi que le display
 function setAllZero(){
@@ -98,26 +36,12 @@ function reset(){
 }
 
 //Push les opérateurs dans userNumber est puis sur le display
-function plus(){
+function usrInpOp(val){
     firstNbr = userNumber.join('')
-    userNumber.push(operatorPlus)
+    userNumber.push(val)
     outputDisplay.innerText = userNumber.join('')
 }
-function minus(){
-    firstNbr = userNumber.join('')
-    userNumber.push(operatorMinus)
-    outputDisplay.innerText = userNumber.join('')
-}
-function multiply(){
-    firstNbr = userNumber.join()
-    userNumber.push(operatorMult)
-    outputDisplay.innerText = userNumber.join('')
-}
-function divide(){
-    firstNbr = userNumber.join()
-    userNumber.push(operatorDiv)
-    outputDisplay.innerText = userNumber.join('')
-}
+
 // utilisation de la fonction for pour prendre la longeur de usernumber
 // est utilisation de la fonction switch qui permet d'évaluer la string est d'effectuer le calcul selon les opérateur trouver
 // -=, +=, *=, /=, sont les opérateur utiliser sur les chiffres
